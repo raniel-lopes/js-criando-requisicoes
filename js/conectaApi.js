64dc38f1f6eb2b028ba56e52d9d6e68e952a4d25
Raniel Lopes <raniel.ldms@gmail.com>
2024-11-08 15:09:10 -0300

    Criação da requisiçao GET

---
+++ js/conectaApi.js
@@ -0,0 +1,11 @@
+async function listaVideos() {
+  //Requisição GET
+  const conexao = await fetch("http://localhost:3000/videos");
+  const conexaoConvertida = await conexao.json();
+
+  return conexaoConvertida;
+}
+
+export const conectaApi = {
+  listaVideos,
+};
